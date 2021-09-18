4b0080a5a329245b3dc0a6ce0c52304b06c21fd4
next-ruan-rita <ruan.rita@next.tec.br>
2021-09-18 20:33:29 -0300

    update

---
+++ env.example.js
@@ -0,0 +1,9 @@
+const env = {
+    "twilio": {
+        "account_sid": "asdasdsadasdas",
+        "service_id": "ISasd4e5bdbe8aasdasd99a4257bcb71be38asdasdasdbb7aa12",
+        "auth_token": "asdasdasdasdasdasdasd"   
+    }
+}
+
+module.exports = env
