48321d333753b458d5c0ceac055f700cbccc50c5
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2021-09-18 20:29:38 -0300

    removing my account data

--- env.js
+++
@@ -1,9 +0,0 @@
-const env = {
-    "twilio": {
-        "account_sid": "ACce85358ceb68f3d9be716ae094c36997",
-        "service_id": "IS4e5bdbe8a99a4257bcb71be38bb7aa12",
-        "auth_token": "1cd708ffea0609625c3f064e7a77cd99"   
-    }
-}
-
-module.exports = env
